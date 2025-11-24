bd09b6040fe769fec07e64c6dff3469a4dd77092
nabeel-weekend <nabeel.weekend@gmail.com>
2025-11-24 19:16:44 +0500

    added assets

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 import { log } from "./functions.js";
 
+const chessBoardElement = document.querySelector("#chess-board")
+
 const chessBoard = [
     [0, 1, 2, 3, 4, 5, 6, 7],
     [8, 9, 10, 11, 12, 13, 14, 15],
@@ -11,8 +13,27 @@ const chessBoard = [
     [56, 57, 58, 59, 60, 61, 62, 63]
 ]
 
+// svgs
+const tileSVG = (color, number) => {
+    return `
+    <svg class="${color}-tile" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
+        <rect x="0" h="0" width="100%" height="100%" />
+    </svg>
+    `
+}
+
 for (let row = 0; row < 8; row++) {
-    log(chessBoard[row])
-    // for (let col = 0; col < 8; col++) {
-    // }
+    // a new span created everytime
+    const rowSpan = document.createElement("span");
+    rowSpan.style.display = "flex";
+    chessBoardElement.appendChild(rowSpan);
+
+    // a tile appended to the 8 spans created
+    for (let col = 0; col < 8; col++) {
+        const positionIndex = chessBoard[row][col];
+        const p = document.createElement("span");
+        const tile = (col + row) % 2 == 0 ? tileSVG("white", col + row) : tileSVG("black", col + row);
+        p.innerHTML = tile;
+        rowSpan.appendChild(p);
+    }
 }
